f9a5c5f9050b498b1572a31cbb16e2e1342ded6c
papapin777 <papapin777@gmail.com>
2019-01-15 18:28:08 -0800

    5.11

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :articles
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+end
